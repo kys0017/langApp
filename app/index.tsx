574b7c7840b5fee9fe78886be14109f45ff36451
kys0017 <kys0017@gmail.com>
2024-12-14 01:03:17 +0900

    ✨ 4.19 Drag and Drops Project part Three - add function to call next card

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components/native';
 import { Ionicons } from '@expo/vector-icons';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Animated, Easing, PanResponder, View } from 'react-native';
+import icons from '@/icons';
+import { IconProps } from '@expo/vector-icons/build/createIconSet';
 
 const BLACK_COLOR = '#1e272e';
 const GREY = '#485460';
@@ -105,7 +107,7 @@ export default function Home() {
               easing: Easing.linear,
               useNativeDriver: true,
             }),
-          ]).start();
+          ]).start(nextIcon);
         } else {
           Animated.parallel([onPressOut, goHome]).start();
         }
@@ -113,6 +115,14 @@ export default function Home() {
     }),
   ).current;
   // State
+  const [index, setIndex] = useState(0);
+  const nextIcon = () => {
+    setIndex(prev => prev + 1);
+    Animated.parallel([
+      Animated.spring(scale, { toValue: 1, useNativeDriver: true }),
+      Animated.spring(opacity, { toValue: 1, useNativeDriver: true }),
+    ]).start();
+  };
   return (
     <Container>
       <Edge>
@@ -127,7 +137,11 @@ export default function Home() {
             opacity,
             transform: [...position.getTranslateTransform(), { scale }],
           }}>
-          <Ionicons name={'beer'} color={GREY} size={76} />
+          <Ionicons
+            name={icons[index] as unknown as IconProps<any>['name']}
+            color={GREY}
+            size={76}
+          />
         </IconCard>
       </Center>
       <Edge>
